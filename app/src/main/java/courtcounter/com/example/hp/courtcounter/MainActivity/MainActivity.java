c7837153ec96c920e866d90577bcb06c9a82b7db
pual035 <ankurpal1997@gmail.com>
2019-01-22 16:38:43 +0530

    Initial Commit

---
+++ app/src/main/java/courtcounter/com/example/hp/courtcounter/MainActivity/MainActivity.java
@@ -0,0 +1,16 @@
+package courtcounter.com.example.hp.courtcounter.MainActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.courtcounter.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+}
